ce9d4f8eaacd23a1b12a8b89cb82ec5dcb01a659
skazi-coursera <shafiuddinkazi@gmail.com>
2024-07-23 15:07:02 -0400

    Final Project completed = Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,40 +1,47 @@
-
 import requests # Import the requests library to handle HTTP requests
 import json # Import the json library to handle json
 
 def emotion_detector(text_to_analyze): # Function - emotion_detector that takes a string input (text_to_analyze)
-    
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' # URL for the emotion detector service
     HEADER = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # Set the Headers required for the HTTP request
     input_json = { "raw_document": { "text": text_to_analyze } } # Create a dictionary with text to analyze
     response = requests.post(URL, json = input_json, headers = HEADER) # Send a POST request to the API with the text and headers
-    formatted_response = json.loads(response.text) # Parsing the JSON response from the API
-    emotion = formatted_response['emotionPredictions'][0] # Set variable to store emotions    
-    anger_score = emotion['emotion']['anger'] # Initialize variables for all emotions
-    
-    # Initialize variables for dominant_emotion
-    dominant_score = anger_score
-    dominant_emotion = "anger"
 
-    disgust_score = emotion['emotion']['disgust']
-    if disgust_score > dominant_score: # If disgust_score is greater than dominant_score, reset dominant_score & dominant_emotion
-        dominant_score = disgust_score
-        dominant_emotion = "disgust"
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text) # Parsing the JSON response from the API
+        emotion = formatted_response['emotionPredictions'][0] # Set variable to store emotions    
+        anger_score = emotion['emotion']['anger'] # Initialize variables for all emotions
+        
+        # Initialize variables for dominant_emotion
+        dominant_score = anger_score
+        dominant_emotion = "anger"
 
-    fear_score = emotion['emotion']['fear']
-    if fear_score > dominant_score: # If fear_score is greater than dominant_score, reset dominant_score & dominant_emotion
-        dominant_score = fear_score
-        dominant_emotion = "fear"
+        disgust_score = emotion['emotion']['disgust']
+        if disgust_score > dominant_score: # If disgust_score is greater than dominant_score, reset dominant_score & dominant_emotion
+            dominant_score = disgust_score
+            dominant_emotion = "disgust"
 
-    joy_score = emotion['emotion']['joy']
-    if joy_score > dominant_score: # If joy_score is greater than dominant_score, reset dominant_score & dominant_emotion
-        dominant_score = joy_score
-        dominant_emotion = "joy"
+        fear_score = emotion['emotion']['fear']
+        if fear_score > dominant_score: # If fear_score is greater than dominant_score, reset dominant_score & dominant_emotion
+            dominant_score = fear_score
+            dominant_emotion = "fear"
 
-    sadness_score = emotion['emotion']['sadness']
-    if sadness_score > dominant_score: # If sadness_score is greater than dominant_score, reset dominant_score & dominant_emotion
-        dominant_score = sadness_score
-        dominant_emotion = "sadness"
+        joy_score = emotion['emotion']['joy']
+        if joy_score > dominant_score: # If joy_score is greater than dominant_score, reset dominant_score & dominant_emotion
+            dominant_score = joy_score
+            dominant_emotion = "joy"
+
+        sadness_score = emotion['emotion']['sadness']
+        if sadness_score > dominant_score: # If sadness_score is greater than dominant_score, reset dominant_score & dominant_emotion
+            dominant_score = sadness_score
+            dominant_emotion = "sadness"
+    elif response.status_code == 400: 
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
 
     # Return a dictionary containing emotion analysis results 
     return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant_emotion}
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-# Import Flask, render_template, request from the flask framework package 
+"""
+server.py
+This module handles the server functionality for the application. It manages 
+incoming requests, processes data, and sends appropriate responses to clients.
+"""
+# Import Flask, render_template, request from the flask framework package
 from flask import Flask, render_template, request
-# Import the emotion_detector function from the package created: 
+# Import the emotion_detector function from the package created:
 from EmotionDetection.emotion_detection import emotion_detector
-
 
 #Initiate the flask app
 app = Flask("Emotion Detector")
@@ -11,30 +15,30 @@ app = Flask("Emotion Detector")
 def emotion_detection():
     ''' This code receives the text from the HTML interface and 
         runs emotion detection over it using emotion_detector()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
+        function. The output returned shows the label and its 
+        confidence score for the provided text.'''
     text_to_analyze = request.args.get("textToAnalyze")
-    if text_to_analyze == "":
-        return "Please input a string"
 
     response = emotion_detector(text_to_analyze)
-    
-    concatenated_string = (
-    f"For the given statement, the system response is "
-    f"'anger': {response['anger']}, "
-    f"'disgust': {response['disgust']}, "
-    f"'fear': {response['fear']}, "
-    f"'joy': {response['joy']}, "
-    f" and 'sadness': {response['sadness']}"
-    f". The dominant emotion is joy.")
+
+    if response['dominant_emotion'] is None:
+        concatenated_string = "Invalid text! Please try again!."
+    else:
+        concatenated_string = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']}, "
+        f" and 'sadness': {response['sadness']}"
+        f". The dominant emotion is joy.")
 
     return concatenated_string
-    
-    
+
 @app.route("/")
 def render_index_page():
-    # This function initiates the rendering of the main application page over the Flask channel
+    '''This function initiates the rendering of the main 
+    application page over the Flask channel'''
     return render_template('index.html')
 
 if __name__ == "__main__":
